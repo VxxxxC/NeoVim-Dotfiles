8868780da4d9b20f3619af0d09ef7b90050e3c0d
Veper(Mac) <veper.chesley@protonmail.com>
2022-11-14 12:45:05 +0800

    fix(adjust-lspsaga-diagnostic-key-'[d'-and-']d'): *

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -10,8 +10,8 @@ saga.init_lsp_saga {
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap
 
-keymap.set('n', ']d', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts) -- Jump and show up the error
-keymap.set('n', '[d', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts) -- Jump and show up the error
+keymap.set('n', '[d', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts) -- Jump and show up the error
+keymap.set('n', ']d', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts) -- Jump and show up the error
 -- Show line diagnostics
 keymap.set("n", "<C-d>", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
 
